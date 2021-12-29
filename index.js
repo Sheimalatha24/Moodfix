82978442437b52abd6f5c74cacd87ce87c1ec102
san-rasheed <67732935+san-rasheed@users.noreply.github.com>
2021-12-29 19:53:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
-import loginRoutes from './routes/login.js';
+import ploginRoutes from './routes/login.js';
+import registerRoutes from './routes/register.js';
+import dloginRoutes from './routes/dlogin.js';
 import userRoutes from './routes/user.js';
+import doctorRoutes from './routes/doctor.js';
+import probroutes from './routes/prob.js';
+import afterconsultroutes from './routes/afterconsult.js';
 connectDB()
 dotenv.config()
 
@@ -13,9 +18,13 @@ const app=express()
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
-
-app.use('/login',loginRoutes); 
+// app.use(express.static("public"));
+app.use('/plogin',ploginRoutes);
+app.use('/dlogin',dloginRoutes);  
+app.use('/register',registerRoutes);  
 app.use('/users',userRoutes);
-
+app.use('/doctors',doctorRoutes);
+app.use('/probdesc',probroutes);
+app.use('/afterconsult',afterconsultroutes);
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
